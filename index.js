f91209b8ed7e04bda2223d10a276b070d43854a6
Gabriel Pereira <gabrielpereira@MacBook-Air-de-Gabriel.local>
2023-01-17 06:54:42 -0300

    feat: Subindo App

---
+++ index.js
@@ -0,0 +1,24 @@
+const { Client } = require('whatsapp-web.js');
+const qrcode = require('qrcode-terminal');
+
+const client = new Client();
+
+client.on('qr', (qr) => {
+    // Generate and scan this code with your phone
+    console.log('QR RECEIVED', qr);
+});
+client.on('qr', qr => {
+    qrcode.generate(qr, {small: true});
+});
+
+client.on('ready', () => {
+    console.log('Client is ready!');
+});
+
+client.on('message', message => {
+    if (message.body == 'oi') {
+        message.reply(`iae ${message?._data?.notifyName}`);
+    }
+});
+
+client.initialize();
