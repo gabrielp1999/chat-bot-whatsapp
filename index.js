a319715cc34d7392b20ebe84edf8a5dcec49c68d
Gabriel Pereira <gabrielpereira@MacBook-Air-de-Gabriel.local>
2023-01-17 06:58:17 -0300

    fix: Correção App

--- index.js
+++ index.js
@@ -4,18 +4,17 @@ const qrcode = require('qrcode-terminal');
 const client = new Client();
 
 client.on('qr', (qr) => {
-    // Generate and scan this code with your phone
     console.log('QR RECEIVED', qr);
-});
-client.on('qr', qr => {
     qrcode.generate(qr, {small: true});
 });
+
 
 client.on('ready', () => {
     console.log('Client is ready!');
 });
 
 client.on('message', message => {
+    console.log(message);
     if (message.body == 'oi') {
         message.reply(`iae ${message?._data?.notifyName}`);
     }
